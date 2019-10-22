29616ee5d72ab7b84ead06a2014acfa16e885990
Ashutosh Pandey <pandey.99@osu.edu>
2019-10-22 10:17:55 -0400

    Delete pipeline_assess.sh

--- scripts/pipeline_assess.sh
+++
@@ -1,8 +0,0 @@
-./crn_test.sh TempAecnn 6 4 best >../logs/TempAecnn_6_4_best_assess_log.txt
-./crn_test.sh TempAecnn 6 4 latest >../logs/TempAecnn_6_4_latest_assess_log.txt
-./crn_test.sh TasNet1 6 4 best >../logs/TasNet1_6_4_best_assess_log.txt
-./crn_test.sh TasNet1 6 4 latest >../logs/TasNet1_6_4_latest_assess_log.txt
-./crn_test.sh TasNet 8 5 best >../logs/TasNet_8_5_best_assess_log.txt
-./crn_test.sh TasNet 8 5 latest >../logs/TasNet_8_5_latest_assess_log.txt
-./crn_test.sh CRN 0 0 best >../logs/CRN_0_0_best_assess_log.txt
-./crn_test.sh CRN 0 0 latest >../logs/CRN_0_0_latest_assess_log.txt
